16ea3112fd957b2715b070ee716ec715655edb51
Ecem Atacli <ecematacli@gmail.com>
2021-01-17 15:56:20 +0100

    Add pictures to the desktop menu

--- my-app/components/layout/header/categoriesMenu/subcategories/Subcategories.styles.ts
+++ my-app/components/layout/header/categoriesMenu/subcategories/Subcategories.styles.ts
@@ -7,11 +7,24 @@ interface StyleProps {
 export const SubcategoryContainer = styled.div<StyleProps>`
   width: 100%;
   margin-top: 0.75rem;
-  height: ${({ isSmallScreen }) => (!isSmallScreen ? '35.3rem' : 'unset')};
+  height: ${({ isSmallScreen }) => (!isSmallScreen ? '42rem' : 'unset')};
   font-size: 15px;
 
   .subcategories {
-    display: ${({ isSmallScreen }) => (!isSmallScreen ? 'flex' : 'block')};
+    height: ${({ isSmallScreen }) => (!isSmallScreen ? '42rem' : 'unset')};
+    margin-top: ${({ isSmallScreen }) => (!isSmallScreen ? '0.75rem' : '2rem')};
+    display: ${({ isSmallScreen }) =>
+      !isSmallScreen ? 'flex' : 'inline-block'};
+    justify-content: space-between;
+
+    .wrapper {
+      display: flex;
+    }
+  }
+
+  .image-wrapper {
+    display: flex;
+    align-items: center;
   }
 
   .subcategory {
@@ -22,9 +35,9 @@ export const SubcategoryContainer = styled.div<StyleProps>`
   }
 
   .subcategory-title {
-    padding-right: 20rem;
+    margin-right: ${({ isSmallScreen }) =>
+      !isSmallScreen ? '20rem' : 'unset'};
     font-weight: bold;
-    display: flex;
     cursor: pointer;
   }
 
@@ -34,7 +47,7 @@ export const SubcategoryContainer = styled.div<StyleProps>`
   }
 
   .level2-subcategory-title {
-    padding: 0.5rem 0;
+    padding: 1rem 0;
     cursor: pointer;
   }
 `;
--- my-app/components/layout/header/categoriesMenu/subcategories/Subcategories.tsx
+++ my-app/components/layout/header/categoriesMenu/subcategories/Subcategories.tsx
@@ -1,16 +1,21 @@
+import classNames from 'classnames';
+
 import { Subcategory } from '../useCategoriesMenu';
 import PaddedLayout from '../../../../paddedLayout/PaddedLayout';
 import { SubcategoryContainer } from './Subcategories.styles';
 import useScreenWidth from '../../../../../hooks/useScreenWidth';
+import Image from 'next/image';
 
 interface Props {
   subcategories: Subcategory[];
   onCategoryItemClick: (categorySlug: string, categoryId: string) => void;
+  openedCategory?: string;
 }
 
 const Subcategories: React.FC<Props> = ({
   subcategories,
   onCategoryItemClick,
+  openedCategory,
 }) => {
   const { isSmallScreen } = useScreenWidth();
 
@@ -19,27 +24,41 @@ const Subcategories: React.FC<Props> = ({
       <div key={s.id} className='second-level-subcategory'>
         <span
           onClick={() => onCategoryItemClick(s.id, s.slug)}
-          className='level2-subcategory-title'
-        >
+          className='level2-subcategory-title'>
           {s.name}
         </span>
       </div>
     ));
   };
+
   return (
     <SubcategoryContainer isSmallScreen={isSmallScreen}>
       <PaddedLayout padding={{ bottom: '0' }}>
         <div className='subcategories'>
-          {subcategories.map(subcategory => (
-            <div key={subcategory.id} className="subcategory">
-              <span
-                onClick={() => onCategoryItemClick(subcategory.id, subcategory.slug)}
-                className='subcategory-title'>
-                {subcategory.name}
-              </span>
-              {renderSecondLevelSubcategory(subcategory)}
+          <div className={classNames({ 'wrapper': !isSmallScreen })}>
+            {subcategories.map(subcategory => (
+              <div key={subcategory.id} className='subcategory'>
+                <span
+                  className='subcategory-title'
+                  onClick={() =>
+                    onCategoryItemClick(subcategory.id, subcategory.slug)
+                  }
+                >
+                  {subcategory.name}
+                </span>
+                {renderSecondLevelSubcategory(subcategory)}
+              </div>
+            ))}
+          </div>
+          {!isSmallScreen &&
+            <div className='image-wrapper'>
+              <Image
+                src={`/images/category-${openedCategory}.jpg`}
+                width='289'
+                height='181'
+              />
             </div>
-          ))}
+          }
         </div>
       </PaddedLayout>
     </SubcategoryContainer>
