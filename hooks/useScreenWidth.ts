ca2b925d6cd36b0f557c6061317046c5c02fc6b7
Ecem Atacli <ecematacli@gmail.com>
2021-02-21 15:23:52 +0100

    Add throttle function for windows resizing as performance improvement

--- hooks/useScreenWidth.ts
+++ hooks/useScreenWidth.ts
@@ -1,16 +1,26 @@
 import { useState, useEffect } from 'react';
+import throttle from 'lodash.throttle';
 
 const useScreenWidth = () => {
   const [screenWidth, setScreenWidth] = useState(null);
 
-  useEffect(() => {
-    const handleResize = () => {
-      setScreenWidth(window.innerWidth);
-    };
+  const throttled = throttle(f => f(), 2000, { leading: true });
 
+  useEffect(() => {
+    let isMounted = true;
+
+    const handleResize = () => {
+      if (isMounted) {
+        throttled(() => setScreenWidth(window.innerWidth));
+      }
+    };
     handleResize();
     window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
+
+    return () => {
+      isMounted = false;
+      window.removeEventListener('resize', handleResize);
+    };
   }, [screenWidth]);
 
   return {
